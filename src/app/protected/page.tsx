0d1f9ea47eb2982c59e92dd53659015cda16636e
Caposto <christiand.apostol@outlook.com>
2024-12-09 18:04:42 -0500

    refactored LogoutButton

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,25 +1,13 @@
 "use client";
 
-import { Button } from "@mantine/core";
+import LogoutButton from "../components/LogoutButton";
 
 export default function Protected() {
-  // TODO: Should logout button be in its own component?
-  const handleLogout = async (event) => {
-    event.preventDefault();
-    try {
-      await fetch("/api/logout", {
-        method: "GET",
-      });
-      console.log("Logged out");
-    } catch (e) {
-      console.error(e);
-    }
-  };
   return (
     <div>
       <h1>Protected Page</h1>
       <p>This page is protected.</p>
-      <Button onClick={handleLogout}>Logout</Button>
+      <LogoutButton />
     </div>
   );
 }
