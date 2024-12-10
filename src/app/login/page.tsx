797e09985b75470f61f44d3a512961364e28be7a
Caposto <christiand.apostol@outlook.com>
2024-12-09 19:36:40 -0500

    Login form styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { isEmail, isNotEmpty, useForm } from "@mantine/form";
-import { TextInput, Group, Button, PasswordInput } from "@mantine/core";
+import { Anchor, TextInput, Group, Button, PasswordInput } from "@mantine/core";
 import { useRouter } from "next/navigation";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase"; // FIXME: Get rid of relative imports
@@ -52,21 +52,60 @@ export default function Login() {
   }
 
   return (
-    <form onSubmit={loginForm.onSubmit((values) => handleSubmit(values))}>
-      <TextInput
-        label="Email"
-        placeholder="knguyen@gmail.com"
-        {...loginForm.getInputProps("email")}
-      />
-      <PasswordInput
-        label="Password"
-        placeholder="password"
-        {...loginForm.getInputProps("password")}
-      />
-      <Group mt="md">
-        <Button type="submit">Submit</Button>
-      </Group>
-      {error && <div>{error}</div>}
-    </form>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-8">
+        <h2 className="text-2xl font-bold text-center text-white">Login</h2>
+        <form
+          className="mt-6"
+          onSubmit={loginForm.onSubmit((values) => handleSubmit(values))}
+        >
+          <div className="mb-4">
+            <TextInput
+              withAsterisk
+              label="Email"
+              placeholder="knguyen@gmail.com"
+              {...loginForm.getInputProps("email")}
+              classNames={{
+                input:
+                  "border-gray-700 bg-gray-700 text-gray-300 rounded-md placeholder-gray-500",
+                label: "text-gray-400 font-medium mb-2",
+              }}
+            />
+          </div>
+          <div className="mb-4">
+            <PasswordInput
+              withAsterisk
+              label="Password"
+              placeholder="password"
+              {...loginForm.getInputProps("password")}
+              classNames={{
+                input:
+                  "border-gray-700 bg-gray-700 text-gray-300 rounded-md placeholder-gray-500",
+                label: "text-gray-400 font-medium mb-2",
+              }}
+            />
+          </div>
+          <div className="flex justify-between items-center text-sm">
+            <Anchor href="/register" className="text-blue-400 hover:underline">
+              Don&apos;t have an account? Create one here.
+            </Anchor>
+          </div>
+          <Group mt="md">
+            <Button
+              type="submit"
+              fullWidth
+              className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+            >
+              Submit
+            </Button>
+          </Group>
+          {error && (
+            <div className="mt-4 bg-red-200 text-red-800 p-2 rounded border border-red-400">
+              {error}
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import { Button } from "@mantine/core";
+import LogoutButton from "./components/LogoutButton";
 
 export default function Home() {
+  // TODO: Show user information from the session here
+  // TODO: Write tests?
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -48,9 +51,8 @@ export default function Home() {
           >
             Read our docs
           </a>
-          <Button>
-            Mantine
-          </Button>
+          <Button>Mantine</Button>
+          <LogoutButton />
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
