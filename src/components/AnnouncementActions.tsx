0cd9ff25f096dc7f9851d161b9d4c6c35b14cb7e
RyanCamburn <61949240+RyanCamburn@users.noreply.github.com>
2024-12-11 13:59:23 -0500

    Tested Locally, everything worked

--- src/components/AnnouncementActions.tsx
+++ src/components/AnnouncementActions.tsx
@@ -1,56 +1,56 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Button } from "@mantine/core";
+import React from 'react';
+import { Button } from '@mantine/core';
 
 // For testing Purposes
 
 const AnnouncementActions: React.FC = () => {
   const handleCreate = async () => {
-    const response = await fetch("/api/announcements/create", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('/api/announcements/create', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        type: "info",
-        message: "New announcement",
+        type: 'info',
+        message: 'New announcement',
         scheduleDate: new Date().toISOString(),
         expirationDate: new Date(new Date().getTime() + 86400000).toISOString(), // +1 day
         active: true,
       }),
     });
     const data = await response.json();
-    console.log("Create response:", data);
+    console.log('Create response:', data);
   };
 
   const handleRead = async () => {
-    const response = await fetch("/api/announcements");
+    const response = await fetch('/api/announcements');
     const data = await response.json();
-    console.log("Read response:", data);
+    console.log('Read response:', data);
   };
 
   const handleUpdate = async () => {
-    const response = await fetch("/api/announcements/update", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('/api/announcements/update', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        id: "gL3XPibWhK3Cpp0MzHt5", // Replace with actual document ID
-        message: "Updated announcement",
+        id: 'qG8p0aMLffoRIzIf7hAT', // Replace with actual document ID
+        message: 'Updated announcement',
       }),
     });
     const data = await response.json();
-    console.log("Update response:", data);
+    console.log('Update response:', data);
   };
 
   const handleDelete = async () => {
-    const response = await fetch("/api/announcements/delete", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('/api/announcements/delete', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        id: "gL3XPibWhK3Cpp0MzHt5", // Replace with actual document ID
+        id: 'qG8p0aMLffoRIzIf7hAT', // Replace with actual document ID
       }),
     });
     const data = await response.json();
-    console.log("Delete response:", data);
+    console.log('Delete response:', data);
   };
 
   return (
