1adddfa39a58af58d1fb25d2c4ca11731e972995
RyanCamburn <61949240+RyanCamburn@users.noreply.github.com>
2024-12-11 11:50:54 -0500

    weeding out bugs and simplified api with data functions

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,3 +1,1 @@
 // TODO: Can we move all the login, register and, logout endpoints here like in the starter example: https://github.com/awinogrodzki/next-firebase-auth-edge/blob/main/examples/next-typescript-starter/api/index.ts
-
-import { NextResponse } from "next/server";
