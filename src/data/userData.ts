328fbd92f5e81da18f2cd897f4e2d8c766176e94
Jack Gibson <94558846+jackfgibson@users.noreply.github.com>
2024-12-14 13:35:10 -0500

    marcusNew/crudUserOperations Issue (#17)
    * created user CRUD ops and made a User type
    
    * updated and tested user crud operations in new format
    
    * fixed firebase.ts
    
    * created user CRUD ops and made a User type
    
    * updated and tested user crud operations in new format
    
    * fixed firebase.ts
    
    * added try/catch to user data functions
    
    * created user CRUD ops and made a User type
    
    * updated and tested user crud operations in new format
    
    * Rebased from main
    
    wq
    
    * created user CRUD ops and made a User type
    
    * updated and tested user crud operations in new format
    
    * fixed firebase.ts
    
    * added try/catch to user data functions
    
    * ran formatter
    
    ---------
    
    Co-authored-by: Caposto <christiand.apostol@outlook.com>

---
+++ src/data/userData.ts
@@ -0,0 +1,80 @@
+import {
+  addDoc,
+  collection,
+  Timestamp,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from 'firebase/firestore';
+import { db } from '../firebase';
+
+interface User {
+  _id?: string;
+  role: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber?: string;
+  gender: string;
+  industry?: string;
+  permissions: string;
+  assignees?: string;
+  createdAt?: Timestamp;
+  updatedAt?: Timestamp;
+}
+
+export async function createUser(
+  user: Omit<User, '_id' | 'createdAt' | 'updatedAt'>,
+): Promise<string> {
+  const newUser = {
+    ...user,
+    createdAt: Timestamp.now(),
+    updatedAt: Timestamp.now(),
+  };
+
+  const docRef = await addDoc(collection(db, 'users'), newUser);
+  return docRef.id;
+}
+
+export async function getAllUsers(): Promise<User[]> {
+  try {
+    const snapshot = await getDocs(collection(db, 'users'));
+    return snapshot.docs.map((doc) => ({ _id: doc.id, ...doc.data() }) as User);
+  } catch (pingus) {
+    throw new Error(`Failed to get all users: ${pingus}`);
+  }
+}
+
+export async function getUserById(id: string): Promise<User | null> {
+  try {
+    const docRef = doc(db, 'users', id);
+    const snapshot = await getDoc(docRef);
+    if (!snapshot.exists()) return null;
+    return { _id: snapshot.id, ...snapshot.data() } as User;
+  } catch (pingus) {
+    throw new Error(`Failed to get user by id: ${pingus}`);
+  }
+}
+
+export async function updateUser(
+  id: string,
+  updatedFields: Partial<Omit<User, '_id' | 'createdAt'>>,
+): Promise<void> {
+  try {
+    const docRef = doc(db, 'users', id);
+    await updateDoc(docRef, { ...updatedFields, updatedAt: Timestamp.now() });
+  } catch (pingus) {
+    throw new Error(`Failed to update user: ${pingus}`);
+  }
+}
+
+export async function deleteUser(id: string): Promise<void> {
+  try {
+    const docRef = doc(db, 'users', id);
+    await deleteDoc(docRef);
+  } catch (pingus) {
+    throw new Error(`Failed to get all users: ${pingus}`);
+  }
+}
