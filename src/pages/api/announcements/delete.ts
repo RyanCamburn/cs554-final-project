fcbc9b5100e0aace81320f0029f8b13d9af480ea
Christian Apostol <christiand.apostol@outlook.com>
2024-12-18 16:41:02 -0500

    added auth layer to announcements endpoints

--- src/pages/api/announcements/delete.ts
+++ src/pages/api/announcements/delete.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { deleteAnnouncement } from '@/data/annoucementData';
+import getUIDandRole from '@/data/serverAuth';
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,6 +8,22 @@ export default async function handler(
 ) {
   if (req.method !== 'DELETE') {
     return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  let role: string;
+
+  try {
+    const data = await getUIDandRole(req);
+    role = data.role;
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ error: 'Not authorized' });
+  }
+
+  if (role !== 'admin') {
+    return res.status(403).json({
+      error: 'Forbidden: You do not have permission to perform this action.',
+    });
   }
 
   const { id } = req.body;
--- src/pages/api/announcements/index.ts
+++ src/pages/api/announcements/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllAnnouncements } from '@/data/annoucementData';
+import getUIDandRole from '@/data/serverAuth';
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,6 +9,23 @@ export default async function handler(
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
+
+  // let uid: string;
+  // let role: string;
+
+  // try {
+  //   const data = await getUIDandRole(req);
+  //   uid = data.uid;
+  //   role = data.role;
+  // } catch (error) {
+  //   console.error(error);
+  //   return res.status(401).json({ error: 'Not authorized' });
+  // }
+
+  // // Everyone who has an
+  // if (!role || !uid) {
+  //   return res.status(403).json({ error: 'Forbidden: You do not have permission to perform this action.' });
+  // }
 
   try {
     const announcements = await getAllAnnouncements();
