13ca50fb31918bfa50d615ff720b0cc771144f2e
Caposto <christiand.apostol@outlook.com>
2024-12-18 02:33:33 -0500

    implemented cache

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllUsers } from '@/data/userData';
+import redisClient from '@/redis';
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,8 +10,15 @@ export default async function handler(
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
+  const cachedUsers = await redisClient.get('users');
+  if (cachedUsers) {
+    console.log('User Cache Hit!');
+    return res.status(200).json(JSON.parse(cachedUsers));
+  }
+
   try {
     const users = await getAllUsers();
+    await redisClient.set('users', JSON.stringify(users), { EX: 3600 });
     res.status(200).json(users);
   } catch (error) {
     console.error(error);
