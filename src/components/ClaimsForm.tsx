21db3e9fd427c4f731403c0739c51b77fdab89bd
Christian Apostol <christiand.apostol@outlook.com>
2024-12-18 12:47:12 -0500

    sucessfully setting claims through form

--- src/components/ClaimsForm.tsx
+++ src/components/ClaimsForm.tsx
@@ -6,8 +6,8 @@ import { notifications } from '@mantine/notifications';
 
 // This component allows admins to elevate a user's role by setting custom claims
 interface ClaimFormValues {
-  uid: string;
-  role: string;
+  uid: string,
+  role: string,
 }
 
 export default function ClaimsForm() {
@@ -27,7 +27,6 @@ export default function ClaimsForm() {
     fetchUser();
   }, []);
 
-  // Since user data is constrained to the Select Component minimal validaiton is needed
   const claimForm: UseFormReturnType<ClaimFormValues> = useForm({
     initialValues: {
       uid: '',
@@ -36,18 +35,18 @@ export default function ClaimsForm() {
     validate: {
       uid: isNotEmpty('User ID is required'),
       role: isNotEmpty('Role is required'),
-    },
+    }
   });
 
-  const handleClaimSubmit = async (values: { uid: string; role: string }) => {
+  const handleClaimSubmit = async (values: { uid: string, role: string }) => {
     const response = await fetch('/api/admin/claims', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        uid: values.uid,
-        claims: { role: values.role },
+        "uid": values.uid,
+        "claims": { role: values.role },
       }),
     });
 
@@ -65,34 +64,26 @@ export default function ClaimsForm() {
   };
 
   return (
-    <form onSubmit={claimForm.onSubmit((values) => handleClaimSubmit(values))}>
+    <form onSubmit={claimForm.onSubmit((values) =>
+      handleClaimSubmit(values),
+    )}>
       <Select
         label="User"
         placeholder="Select a user"
         required
-        searchable
         {...claimForm.getInputProps('uid')}
-        data={users
-          .filter((user) => user._id)
-          .map((user) => ({
-            value: user._id as string,
-            label: `${user.firstName} ${user.lastName} (${user.role})`, // i.e "John Smith (mentor)"
-          }))}
+        data={users.filter(user => user._id).map((user) => ({ value: user._id as string, label: `${user.firstName} ${user.lastName} (${user.role})` }))}
       />
       <Select
         label="Role"
         placeholder="Select a role"
         required
-        data={[
-          { value: 'admin', label: 'Admin' },
-          { value: 'mentor', label: 'Mentor' },
-          { value: 'mentee', label: 'Mentee' },
-        ]}
+        data={[{ value: 'admin', label: 'Admin' }, { value: 'mentor', label: 'Mentor' }, { value: 'mentee', label: 'Mentee' }]}
         {...claimForm.getInputProps('role')}
       />
       <Button type="submit" color="blue">
         Set User Claims
       </Button>
     </form>
-  );
+  )
 }
