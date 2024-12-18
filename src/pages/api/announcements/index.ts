e1fda85b30aa6359792895b3357b072dfa0fd6af
Christian Apostol <christiand.apostol@outlook.com>
2024-12-18 17:33:56 -0500

    removed repeat dependencies

--- src/pages/api/announcements/index.ts
+++ src/pages/api/announcements/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllAnnouncements } from '@/data/annoucementData';
 import getUIDandRole from '@/data/serverAuth';
+import redisClient from '@/cache';
 
 export default async function handler(
   req: NextApiRequest,
@@ -22,13 +23,23 @@ export default async function handler(
     return res.status(401).json({ error: 'Not authorized' });
   }
 
-  // Everyone who has an
+  // Check cache
+  const cachedAnnouncements = await redisClient.get('announcements');
+  if (cachedAnnouncements) {
+    console.log('Announcement Cache Hit!');
+    return res.status(200).json(JSON.parse(cachedAnnouncements));
+  }
+
+  // Everyone who has an account should be able to read the events
   if (!role || !uid) {
-    return res.status(403).json({ error: 'Forbidden: You do not have permission to perform this action.' });
+    return res.status(403).json({
+      error: 'Forbidden: You do not have permission to perform this action.',
+    });
   }
 
   try {
     const announcements = await getAllAnnouncements();
+    await redisClient.set('users', JSON.stringify(announcements), { EX: 3600 });
     res.status(200).json(announcements);
   } catch (error) {
     console.error(error);
